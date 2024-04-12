e7259bd926fedb13f1fc356f52bed715dc576cc8
McAshesha <mcashesha@mail.ru>
2024-04-12 16:28:33 +0300

    perf(mandelbrot): widen viewport and bump iteration cap to 100

--- src/Fractals/Generator/Mandelbrot.hs
+++ src/Fractals/Generator/Mandelbrot.hs
@@ -9,13 +9,13 @@ import Data.Complex (Complex(..), magnitude)
 
 generate :: TerminalSize -> [[Color]]
 generate (TerminalSize (rows, cols)) =
-  [ [ Color (mandelbrotIter (x col :+ y row)) 50
+  [ [ Color (mandelbrotIter (x col :+ y row)) 100
     | col <- [0..cols-1] ]
   | row <- [0..rows-1] ]
   where
     x col
-      | cols <= 1  = -2.0
-      | otherwise  = -2.0 + (fromIntegral col / fromIntegral (cols - 1)) * 3.0
+      | cols <= 1  = -2.5
+      | otherwise  = -2.5 + (fromIntegral col / fromIntegral (cols - 1)) * 3.5
     y row
       | rows <= 1  = 1.0
       | otherwise  = 1.0 - (fromIntegral row / fromIntegral (rows - 1)) * 2.0
@@ -24,6 +24,6 @@ mandelbrotIter :: Complex Double -> Int
 mandelbrotIter c = go 0 0
   where
     go z iter
-      | iter >= 50 = 50
+      | iter >= 100 = 100
       | magnitude z > 2 = iter
       | otherwise = go (z^2 + c) (iter + 1)
