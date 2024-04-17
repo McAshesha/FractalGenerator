b569bb56df07bcc2f108b90c59d5d48e6f5a2cd4
McAshesha <mcashesha@mail.ru>
2024-04-17 21:24:54 +0300

    feat(main): wire fractal menu loop into entry point

--- app/Main.hs
+++ app/Main.hs
@@ -1,11 +1,38 @@
 module Main where
 
+import CLI.UI (displayWelcomeScreen, displayFractalMenu, promptContinue, displayConditions, displayResultFractal, displayErrorSizeMsg)
+import Utils (getTerminalSize)
 import Control.Exception (bracket)
 import System.Console.ANSI (hideCursor, showCursor)
 
 main :: IO ()
 main = bracket setup teardown $ \_ -> do
-  putStrLn "FractalGenerator (work in progress)"
+  start
+  loop
+  where
+    start = do
+      maybeSize <- getTerminalSize
+      case maybeSize of
+        Just size -> do
+          displayWelcomeScreen size
+          displayConditions
+        Nothing -> do
+         displayErrorSizeMsg
+         continue <- promptContinue
+         if continue then start else pure ()
+
+    loop = do
+      maybeSize <- getTerminalSize
+      case maybeSize of
+        Just size -> do
+          choice <- displayFractalMenu size
+          displayResultFractal choice size
+          continue <- promptContinue
+          if continue then loop else pure ()
+        Nothing -> do
+          displayErrorSizeMsg
+          continue <- promptContinue
+          if continue then loop else pure ()
 
 setup :: IO ()
 setup = do
